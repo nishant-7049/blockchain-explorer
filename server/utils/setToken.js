842432c656f009482f245d395c42cb0971c66d99
nishant-7049 <nishantsharmay2@gmail.com>
2024-02-02 20:32:48 +0530

    changed cookie options

--- server/utils/setToken.js
+++ server/utils/setToken.js
@@ -4,9 +4,12 @@ exports.setToken = async (res, user, message) => {
   const token = jwt.sign({ id: user._id }, process.env.SECRET, {
     expiresIn: "2h",
   });
-  res.cookie("token", token, { httpOnly: true }).status(200).json({
-    success: true,
-    user,
-    message,
-  });
+  res
+    .cookie("token", token, { httpOnly: true, secure: true })
+    .status(200)
+    .json({
+      success: true,
+      user,
+      message,
+    });
 };
