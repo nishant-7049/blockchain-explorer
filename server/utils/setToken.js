35ce6a751dd93504ca87fbd29fc471a34b764be5
nishant-7049 <nishantsharmay2@gmail.com>
2024-02-02 20:47:51 +0530

    again set cookie options

--- server/utils/setToken.js
+++ server/utils/setToken.js
@@ -5,7 +5,15 @@ exports.setToken = async (res, user, message) => {
     expiresIn: "2h",
   });
   res
-    .cookie("token", token, { httpOnly: true, secure: true, sameSite: "None" })
+    .cookie("token", token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      path: "/",
+      sameSite: "None",
+      secure: true,
+      httpOnly: true,
+    })
     .status(200)
     .json({
       success: true,
