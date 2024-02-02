592be4377f61d4411bd880e40d6a0073622e6a80
nishant-7049 <nishantsharmay2@gmail.com>
2024-02-02 21:20:15 +0530

    token expire removed

--- server/utils/setToken.js
+++ server/utils/setToken.js
@@ -6,9 +6,6 @@ exports.setToken = async (res, user, message) => {
   });
   res
     .cookie("token", token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
       path: "/",
       sameSite: "None",
       secure: true,
